4cc38de451fd50b6faa799402c0a94c1f6d49768
Stephen Rhoton <stephen.rhoton@dealerware.com>
2023-01-12 06:31:16 -0500

    Syntax fix

--- app_infrastructure/appstack/data.tf
+++ app_infrastructure/appstack/data.tf
@@ -6,42 +6,42 @@ data "aws_vpc" "b2c_vpc" {
 }
 
 data "aws_subnet" "private_1" {
-  vpc_id = data.aws_vpc.b2c_vpc.vpc_id
+  vpc_id = data.aws_vpc.b2c_vpc.id
   filter {
     name = "tag:Name"
     values = ["private-1"]
   }
 }
 data "aws_subnet" "private_2" {
-  vpc_id = data.aws_vpc.b2c_vpc.vpc_id
+  vpc_id = data.aws_vpc.b2c_vpc.id
   filter {
     name = "tag:Name"
     values = ["private-2"]
   }
 }
 data "aws_subnet" "private_3" {
-  vpc_id = data.aws_vpc.b2c_vpc.vpc_id
+  vpc_id = data.aws_vpc.b2c_vpc.id
   filter {
     name = "tag:Name"
     values = ["private-3"]
   }
 }
 data "aws_subnet" "public_1" {
-  vpc_id = data.aws_vpc.b2c_vpc.vpc_id
+  vpc_id = data.aws_vpc.b2c_vpc.id
   filter {
     name = "tag:Name"
     values = ["public-1"]
   }
 }
 data "aws_subnet" "public_2" {
-  vpc_id = data.aws_vpc.b2c_vpc.vpc_id
+  vpc_id = data.aws_vpc.b2c_vpc.id
   filter {
     name = "tag:Name"
     values = ["public-2"]
   }
 }
 data "aws_subnet" "public_3" {
-  vpc_id = data.aws_vpc.b2c_vpc.vpc_id
+  vpc_id = data.aws_vpc.b2c_vpc.id
   filter {
     name = "tag:Name"
     values = ["public-3"]
